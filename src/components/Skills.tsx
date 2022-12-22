b89670f5bdd7f51e1aa470e2bde57326db139789
yeremiaChris <yeremia997@gmail.com>
2022-12-22 19:53:42 +0700

    update recentWorks component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,9 +4,11 @@ function Skills() {
     <div className="mt-10 lg:mt-20">
       <h2 className="text-2xl font-medium text-green-500">skills()</h2>
 
-      <div className="flex items-center justify-center lg:justify-start gap-28 flex-wrap mt-14">
+      <div className="flex items-center justify-center lg:justify-start gap-20 flex-wrap mt-14">
         {skillsStack?.map((pElement) => (
-          <img src={`/img/${pElement}.png`} key={pElement} alt={pElement} className="w-20" />
+          <div className="w-24 box-center h-24 rounded-full shadow-lg p-4">
+            <img src={`/img/${pElement}.png`} key={pElement} alt={pElement} />
+          </div>
         ))}
       </div>
     </div>
--- src/constants/recentWorks.ts
+++ src/constants/recentWorks.ts
@@ -2,21 +2,24 @@ export const works = [
   {
     title: "MAMI (Manulife Asset Manajemen Indonesia) Mutual Funds Site + CMS",
     image: "mami.png",
+    link: "https://reksadana-manulife.com/",
   },
   {
     title: "Social Media Scrapping Site",
     image: "scrapping.png",
+    link: "https://fastr.id/",
   },
   {
     title: "FLOU (Indonesia Leading & Reliable Cloud Service Provider) Cloud Site + CMS",
     image: "flou.png",
+    link: "https://floucloud.id/",
   },
   {
     title: "Dashbourd Purity (Slicing PURITY UI)",
     image: "purity.png",
   },
   {
-    title: "Revamp Kiriminaja.com site with Dark Mode Feature",
+    title: "Revamp Kiriminaja.com site",
     image: "kiriminaja.png",
   },
   {
